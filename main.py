7447c6d375dbbb15a2cabb71de776c025bcd7436
ShawnRSidwell <shawnrsidwell@gmail.com>
2023-12-05 18:29:50 -0800

    Simple python game where you try to find the person with the higher followers

---
+++ main.py
@@ -0,0 +1,64 @@
+from art import logo, vs
+from game_data import data
+from random import choice
+from os import system
+
+def get_person():
+    return choice(data)
+
+
+def display_person(person, letter):
+    print(
+        f"Person {letter}: {person['name']}, a {person['description']}, from {person['country']}"
+    )
+
+
+def display_choices(person1, person2, score):
+    system('cls')
+    print(logo)
+    print("")
+    if score > 0:
+        print(f"You're right! Current score: {score}")
+    else:
+        print(f"Current Score: {score}")
+    display_person(person1, "A")
+    print(vs)
+    display_person(person2, "B")
+
+
+def check_higher(person1, person2):
+    if person1["follower_count"] > person2["follower_count"]:
+        return 'a'
+    else:
+        return 'b'
+    
+
+def play_game(score):
+    game_over = False
+    while not game_over:
+        person1 = get_person()
+        person2 = get_person()
+        while person1 == person2:
+            person2 = get_person()
+        display_choices(person1, person2, score)
+        player_choice = input("Who has more followers? Type 'A' or 'B'\n").lower()
+        winner = check_higher(person1, person2)
+        if player_choice == winner:
+            score += 1
+        else:   
+            system('cls')
+            print(logo)
+            print(f"Sorry, that's incorrect. Final Score: {score}")
+            game_over = True
+
+
+
+bored = False
+while not bored:
+    play_game(score = 0)
+    again = input("Play again? Y or N\n").lower()
+    if again == 'n':
+        system('cls')
+        print("Quiting....")
+        print("Have a good day!")
+        bored = True  
